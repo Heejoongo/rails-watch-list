f331e6e2e297d9eb7a9deb07127c3946d87b1057
Hiedjoung O <hiedjoung.o@gmail.com>
2023-08-18 14:42:53 +0200

    hiroku

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  NAMES = %w( action adventure comedy drama fantasy horror musicals mystery romance science-fiction sports thriller Western)
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
